91a1399831e2c68e553e19112453641b7beebd5d
Chandler Warren <chand@chandlers-air.lan>
2023-07-16 10:17:49 -0400

    fix to footer on mobile

--- components/footer.php
+++ components/footer.php
@@ -1,13 +1,10 @@
-<footer class="bg-slate-100 dark:bg-gray-900 fixed bottom-0 w-full h-15">
-    <div class="container flex flex-col justify-center items-center p-2 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
+<footer class="bg-slate-100 dark:bg-gray-900 fixed bottom-0 w-full h-10">
+    <div class="flex flex-row justify-center p-2 mx-auto">
         <p class="text-xs text-gray-900 dark:text-gray-300 mx-4">© 2023 Chandler Warren</p>
-        <div class="mb-1 right-0  mr-4 z-10">
-            <div>
-                <a title="View the PAT stack" href="https://github.com/cwarren714/chandler-site" target="_blank" class="block w-6 h-6 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
-                    <img class="object-cover object-center w-full h-full rounded-full" x-bind:src="darkMode==='dark' ? './assets/github-mark-white.svg' : './assets/github-mark.svg'" />
-                </a>
-            </div>
-        </div>
+        <a title="View the PAT stack" href="https://github.com/cwarren714/chandler-site" target="_blank" class="w-6 h-6 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+            <img class="object-cover object-center w-full h-full rounded-full" x-bind:src="darkMode==='dark' ? './assets/github-mark-white.svg' : './assets/github-mark.svg'" />
+        </a>
+
     </div>
 </footer>
 
