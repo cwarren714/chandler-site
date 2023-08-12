860cd75718e9a202f9f7c4327b130768ffe03e69
Chandler Warren <chandler.warren@uconn.edu>
2023-08-12 12:28:43 -0400

    small change and new favicon

--- views/cod.php
+++ views/cod.php
@@ -6,7 +6,7 @@ $diablo_color = 'sandybrown';
 <body class="bg-gray-100 dark:bg-gray-800 dark:text-slate-300 w-full">
     <div class="flex flex-col justify-center max-w-xl mx-4 md:mx-auto text-center my-6">
         <article class="flex justify-center mt-5 text-justify prose prose-l dark:prose-invert">
-            <p>We all know there are only two types of games: <i>cards</i> or <i>Diablo</i>.
+            <p>We all know there are only two types of games: <i>cards</i> or <i>Diablo</i>. Below is the current breakdown. </p>
         </article>
         <div class="flex justify-center mt-10 prose prose-l dark:prose-invert">
             <div class="flex flex-col">
